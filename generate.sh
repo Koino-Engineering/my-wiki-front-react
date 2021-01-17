cfa7998727bd3475313e848c400c0e597bd6cc5e
KoinoEngineering <email@koino.engineering>
2021-01-18 00:59:24 +0900

    #1 設定をいろいろ修正

--- generate.sh
+++ generate.sh
@@ -10,10 +10,10 @@ echo "compose up"
 ./dc.sh up -d --remove-orphans
 
 echo "waiting up end"
-sleep 60
+sleep 30
 
 echo "Regeneration"
-./dc.sh exec api swagger-codegen generate -i http://back:5000/swagger.json -l typescript-fetch -o ./src/modules/
+./dc.sh exec api generate -i http://back:5000/swagger.json -l typescript-fetch -o ./src/modules/api
 
 echo "compose down"
 ./dc.sh down
